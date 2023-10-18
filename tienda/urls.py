c63d622e026f1a016ff7c311e7d913e9f491aa68
Adr1ni <adriano.gongora@tecsup.edu.pe>
2023-10-18 16:51:59 -0500

    Laboratorio 8

--- tienda/urls.py
+++ tienda/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
-
-from . import views
-
-app_name = 'tienda'
-
+from tienda import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('producto/<int:producto_id>',views.producto,name='producto'),
-    path('categoria/<int:categoria_id>',views.categoria,name='categoria'),
+    path('',views.IndexView.as_view(),name='index'),
+    path('categoria',views.CategoriaView.as_view(),name='categorias'),
+    path('categoria/<int:serie_id>',views.CategoriaDetailView.as_view()),
 
+    path('producto',views.ProductoView.as_view(),name='productos'),
+    path('producto/<int:serie_id>',views.ProductoDetailView.as_view()),
 ]
--- tienda/views.py
+++ tienda/views.py
@@ -1,26 +1,83 @@
-from django.shortcuts import get_object_or_404, render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
 from .models import *
-
-def index(request):
-    product_list = Producto.objects.order_by('nombre')[:6]
-    lista_categorias = Categoria.objects.all()
-    context = {
-        'productos':product_list,
-        'categorias':lista_categorias
-    }
-    return render(request,'tienda/index.html', context)
-
-def producto(request,producto_id):
-    producto = get_object_or_404(Producto, pk=producto_id)
-    return render(request,'tienda/producto.html', {'producto' : producto})
+from .serializer import *
 
 
-def categoria(request, categoria_id):
-    categoria = Categoria.objects.get(pk=categoria_id)
-    producto_categoria = Producto.objects.filter(categoria = categoria)
-    return render(request,'tienda/categoria_producto.html', {
-        "producto_categoria":producto_categoria,
-        "categoria":categoria
-    })
+class IndexView(APIView):
+    
+    def get(self,request):
+        context = {'mensaje':'servidor activo'}
+        return Response(context)
+    
+
+class CategoriaView(APIView):
+    def get(self,request):
+        dataSeries = Categoria.objects.all()
+        serSeries = CategoriaSerializer(dataSeries,many=True)
+        return Response(serSeries.data)
+    
+    def post(self,request):
+        serSerie = CategoriaSerializer(data=request.data)
+        serSerie.is_valid(raise_exception=True)
+        serSerie.save()
+        
+        return Response(serSerie.data)
 
 
+class CategoriaDetailView(APIView):
+    
+    def get(self,request,serie_id):
+        dataSerie = Categoria.objects.get(pk=serie_id)
+        serSerie = CategoriaSerializer(dataSerie)
+        return Response(serSerie.data)
+    
+    def put(self,request,serie_id):
+        dataSerie = Categoria.objects.get(pk=serie_id)
+        serSerie = CategoriaSerializer(dataSerie,data=request.data)
+        serSerie.is_valid(raise_exception=True)
+        serSerie.save()
+        return Response(serSerie.data)
+    
+    def delete(self,request,serie_id):
+        dataSerie = Categoria.objects.get(pk=serie_id)
+        serSerie = CategoriaSerializer(dataSerie)
+        dataSerie.delete()
+        return Response(serSerie.data)
+    
+
+class ProductoView(APIView):
+    def get(self,request):
+        dataSeries = Producto.objects.all()
+        serSeries = ProductoSerializer(dataSeries,many=True)
+        return Response(serSeries.data)
+    
+    def post(self,request):
+        serSerie = ProductoSerializer(data=request.data)
+        serSerie.is_valid(raise_exception=True)
+        serSerie.save()
+        
+        return Response(serSerie.data)
+
+
+class ProductoDetailView(APIView):
+    
+    def get(self,request,serie_id):
+        dataSerie = Producto.objects.get(pk=serie_id)
+        serSerie = ProductoSerializer(dataSerie)
+        return Response(serSerie.data)
+    
+    def put(self,request,serie_id):
+        dataSerie = Producto.objects.get(pk=serie_id)
+        serSerie = ProductoSerializer(dataSerie,data=request.data)
+        serSerie.is_valid(raise_exception=True)
+        serSerie.save()
+        return Response(serSerie.data)
+    
+    def delete(self,request,serie_id):
+        dataSerie = Producto.objects.get(pk=serie_id)
+        serSerie = ProductoSerializer(dataSerie)
+        dataSerie.delete()
+        return Response(serSerie.data)
